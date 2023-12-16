e6f3f28235300dfb876ba6874b6c8374285c635d
Mohammad <Mohammad-996-@hotamil.com>
2023-12-16 17:28:18 +0400

    adding dashboard vue

---
+++ src/views/dashBoard.vue
@@ -0,0 +1,17 @@
+<template>
+  <zebraTable></zebraTable>
+</template>
+
+<script>
+import zebraTable from '@/components/zebraTable.vue';
+export default {
+    components:{
+        zebraTable
+    }
+
+}
+</script>
+
+<style>
+
+</style>
