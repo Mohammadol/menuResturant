b04aaa623e71cb03c1b3b908fb54cb46a990ec51
Mohammad <Mohammad-996-@hotamil.com>
2023-12-21 02:49:53 +0400

    resturant page baradd

--- src/components/RnavBar.vue
+++ src/components/RnavBar.vue
@@ -22,7 +22,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .topnav {
   overflow: hidden;
   background-color: #bc0e4c;
@@ -81,6 +81,7 @@ export default {
     float: none;
     display: block;
     text-align: left;
+    border-bottom: 1px solid gray;
   }
 }
 </style>
